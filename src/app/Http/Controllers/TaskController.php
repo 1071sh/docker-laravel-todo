71a6d238702ef0facf0b488f158407bd68d39740
nishida <k.nishida@vistage-llc.com>
2020-06-14 21:38:43 +0900

    chapter7

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
-use App\Task;
 use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -13,10 +14,10 @@ class TaskController extends Controller
     {
         // すべてのフォルダを取得する
         $folders = Folder::all();
-    
+
         // 選ばれたフォルダを取得する
         $current_folder = Folder::find($id);
-    
+
         // 選ばれたフォルダに紐づくタスクを取得する
         $tasks = $current_folder->tasks()->get();
 
@@ -27,9 +28,6 @@ class TaskController extends Controller
         ]);
     }
 
-    /**
-     * GET /folders/{id}/tasks/create
-     */
     public function showCreateForm(int $id)
     {
         return view('tasks/create', [
@@ -48,7 +46,29 @@ class TaskController extends Controller
         $current_folder->tasks()->save($task);
 
         return redirect()->route('tasks.index', [
-        'id' => $current_folder->id,
-    ]);
+            'id' => $current_folder->id,
+        ]);
+    }
+
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
+
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+        $task = Task::find($task_id);
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
+        ]);
     }
 }
